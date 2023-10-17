17c98a12d0a4f7c92419740dfafbab6652053e2a
ben <bgwirema@mtu.edu>
2023-10-17 17:30:51 -0400

    Noah's state class with Ben's graph and infections

--- src/State.java
+++ src/State.java
@@ -1,40 +1,59 @@
 import java.util.ArrayList;
 public class State {
+    private int population;
+    private int infected;
+    private int dead;
+    private double infectionRate;
+    private double mortalityRate;
 
-    String name;
-    int alive;
-    int dead;
-    int infected;
-    ArrayList<State> neighbors;
-    private int pushed;
+    private ArrayList<State> neighbors;
+    private int newCases;
 
-    public void infectNeighbors(){
-
+    public State(int population) {
+        this.population = population;
+        this.infected = 0;
+        this.dead = 0;
+        this.infectionRate = 0.1;
+        this.mortalityRate = 0.01;
     }
 
-    public void push(int newCases){
+    public int getPopulation() {
+        return population;
+    }
 
+    public int getInfected() {
+        return infected;
+    }
+
+    public void infect(int cases){
+        newCases += infectionRate * cases;
+    }
+
+    private void increaseInfection() {
+        infected += newCases;
+        newCases = 0;
+        if (infected > population) {
+            infected = population;
+        }
+    }
+
+    public double getInfectedPercentage() {
+        return (double) infected / population;
+    }
+
+    public void infectNeighbors(){
+        for (State state : neighbors){
+            state.infect(infected);
+        }
     }
 
     public void update(){
-        death();
-        infectionFromNeighbors();
-        infectionFromSelf();
-    }
+        int death = (int) (mortalityRate * infected);
+        population -= death;
+        infected -= death;
+        dead += death;
+        newCases += infectionRate * infected;
 
-    private void death(){
-
-    }
-
-    private void infectionFromNeighbors(){
-
-    }
-
-    private void infectionFromSelf(){
-
-    }
-
-    public String toString(){
-        return name+":"+String.valueOf(infected);
+        increaseInfection();
     }
 }
