1984e7dff04bdae49dae57eab453aacf927eb0d1
Ben Wireman <bgwirema@c28-0113-24.ad.mtu.edu>
2023-10-29 18:22:59 -0400

    Import from Preset File - Sim and State

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,12 +1,50 @@
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Simulation {
     HashMap<String, State> states;
     double infectionRate;
     double mortalityRate;
 
     //construction from file
-    public Simulation(){
-        //constructs the state class variables
+    public Simulation(String filePath){
+        //create scanner from file
+        File file = new File(filePath);
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+        } catch(FileNotFoundException e){
+            //invalid file path caused program to terminate
+            System.out.println("Invalid File Path");
+            System.exit(1);
+        }
+
+        states = new HashMap<String,State>();
+        
+        try {
+            while (sc.hasNextLine()) {
+                String[] header = sc.nextLine().split(":");
+                switch (header[0].trim()) {
+                    case "Simulation" -> {
+                        //initialize simulation parameters
+                        this.infectionRate = Double.parseDouble(sc.nextLine().split("=")[1].trim());
+                        this.mortalityRate = Double.parseDouble(sc.nextLine().split("=")[1].trim());
+                    }
+                    case "State" -> {
+                        //initialize state and put it into hash map
+                        String name = header[1].trim();
+                        states.put(name, new State(sc));
+                    }
+                }
+            }
+        } catch(Exception e){
+            //invalid syntax causes program to terminate
+            System.out.println("Invalid Preset File Syntax");
+            System.exit(1);
+        }
+
     }
 
     //called from GUI
--- src/State.java
+++ src/State.java
@@ -1,11 +1,24 @@
-
+import java.util.Scanner;
 public class State {
     int population;
     int infected;
     String[] neighbors;
 
     //constructed from file
-    public State(){
+    public State(Scanner sc){
+        try{
+            this.population = Integer.parseInt(sc.nextLine().split("=")[1].trim().replace(",",""));
+            this.infected = Integer.parseInt(sc.nextLine().split("=")[1].trim().replace(",",""));
+            this.neighbors = sc.nextLine().split("=")[1].split(",");
+            assert neighbors.length > 0;
+            for (int i=0; i<neighbors.length; i++) {
+                neighbors[i] = neighbors[i].trim();
+            }
+        } catch (Exception e){
+            //invalid syntax causes program to terminate
+            System.out.println("Invalid Preset File Syntax");
+            System.exit(1);
+        }
     }
 
     //put some methods in here to change the population and infected people
