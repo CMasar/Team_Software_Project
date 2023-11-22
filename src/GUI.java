fa8692c2716650f5397e98f1fe7b040ee1ba9659
Noah Provenzola <122239465+njproven@users.noreply.github.com>
2023-11-22 13:43:52 -0500

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -109,6 +109,10 @@ public class GUI extends JPanel {
     private void buildButtonPanel() {
         buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
+        JTextField fileTextField = new JTextField("Enter File Name or Location");
+        fileTextField.setPreferredSize(new Dimension(200, 25));
+        buttonPanel.add(fileTextField);
+        
         // Create start button
         JButton startButton = new JButton("Start Simulation");
         buttonPanel.add(startButton);
